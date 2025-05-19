02d4f47362aafe96ece5104d29775ae85289a026
Eacaw <davidpinchen@gmail.com>
2025-05-19 21:27:30 +0100

    Refactor PlatformController: Enhance obstacle position handling and adjust speed

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlatformController : MonoBehaviour
 {
-    public float speed = 8f;
+    public float speed = 10f;
     public bool isInitialPlatform = false;
+
+    [SerializeField]
+    private bool[] RightLane = new bool[8];
+
+    [SerializeField]
+    private bool[] MiddleLane = new bool[8];
+
+    [SerializeField]
+    private bool[] LeftLane = new bool[8];
+
+    public bool[,] obstaclePositions
+    {
+        get
+        {
+            bool[,] combined = new bool[8, 3];
+            bool[][] rows = { RightLane, MiddleLane, LeftLane };
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    combined[i, j] = rows[j][i];
+                }
+            }
+            return combined;
+        }
+    }
 
     void Update()
     {

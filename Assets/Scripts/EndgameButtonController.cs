ce3fcb548a23d5b2cec874d41047d43ad89557f4
Mateusz Krawczyk <65076733+Mati4790K@users.noreply.github.com>
2025-05-20 20:56:36 +0100

    Add script to manage endgame buttons

---
+++ Assets/Scripts/EndgameButtonController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndgameButtonController : MonoBehaviour
+{
+    [SerializeField] private Button tryAgainButton;
+    [SerializeField] private Button mainMenuButton;
+
+    private void Start()
+    {
+        tryAgainButton.onClick.AddListener(() => LoadScene(SceneManager.GetActiveScene().name));
+        mainMenuButton.onClick.AddListener(() => LoadScene("MainMenu"));
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Clicked button");
+    }
+}
